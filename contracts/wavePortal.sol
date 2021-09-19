2f057e008fdeb6bd088c09d7175258157c9ac889
Matthew Pfeifer <matthewpfeifer@Matthews-MacBook-Pro.local>
2021-09-19 14:49:17 -0400

    moved contract into its own folder

---
+++ contracts/wavePortal.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+import "hardhat/console.sol";
+
+contract WavePortal {
+  // struct is an object w definable attributes
+  struct Wave {
+    string message;
+    address waver;
+    uint timestamp;
+    bool winner;
+    uint userWaveCount;
+  }
+
+  event NewWave(
+    string message,
+    address indexed waver,
+    uint timestamp,
+    bool winner,
+    uint userWaveCount
+  );
+
+  uint waveCount;
+
+  Wave[] waves;
+  uint private rngSeed;
+
+  mapping(address => uint) private cooldown;
+  mapping(address => uint) userWaves;
+
+  constructor() payable {
+  }
+
+  function wave(string memory _message) public {
+    // user cannot wave more than every 15s
+    require(cooldown[msg.sender] + 15 seconds < block.timestamp, "Wait 15s.");
+    // 50% chance to pull "random" number less than 50
+    rngSeed = (block.difficulty + block.timestamp + rngSeed) % 100;
+    bool winner = rngSeed < 50;
+    if (winner) {
+      uint prize = 0.0001 ether;
+      require(prize <= address(this).balance, "Not enough funds for prize");
+      // unsure of how the following line gives the user ether
+      (bool ok,) = (msg.sender).call{ value: prize }("");
+      require(ok, "Failed to give out prize");
+    }
+
+    waveCount += 1;
+    userWaves[msg.sender] += 1;
+    waves.push(Wave(_message, msg.sender, block.timestamp, winner, userWaves[msg.sender]));
+    cooldown[msg.sender] = block.timestamp;
+    emit NewWave(_message, msg.sender, block.timestamp, winner, userWaves[msg.sender]);
+  }
+
+  function allWaves() view public returns (Wave[] memory) {
+    return waves;
+  }
+
+  function totalWaves() view public returns (uint) {
+    return waveCount;
+  }
+}
